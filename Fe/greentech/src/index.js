6c9332f95b4b2e274515dfc47ba1706d2d61e368
TLien <Dtlienk20@gmail.com>
2025-11-01 10:01:12 +0700

    Merge branch b4 into main

--- Fe/greentech/src/index.js
+++ Fe/greentech/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import RouterCustom from "./router";
+import "./style/style.scss";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
--- Fe/greentech/src/pages/user/theme/header/index.js
+++ Fe/greentech/src/pages/user/theme/header/index.js
@@ -1,7 +1,54 @@
 import { memo } from "react";
+import "./style.scss";
+import { AiOutlineFacebook, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineMail, AiOutlineTwitter, AiOutlineUser } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { formater } from "utils/fomater";
 
 const Header = () => {
-    return <h1>Header</h1>;
+    return (
+        <div className="header_top">
+            <div className="container">
+                <div className="row">
+                    <div className="col-6 header_top_left">
+                        <ul>
+                            <li>
+                                <AiOutlineMail />   Dtlienk2@gmail.com
+                            </li>
+                            <li>Miễn phí ship đơn từ {formater(200000)}</li>
+                        </ul>
+                    </div>
+                    <div className="col-6 header_top_right">
+                        <ul>
+                            <li>
+                                <Link to={""}>
+                                    <AiOutlineFacebook />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={""}>
+                                    <AiOutlineInstagram />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={""}>
+                                    <AiOutlineLinkedin />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={""}>
+                                    <AiOutlineTwitter />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={""}>
+                                    <AiOutlineUser />
+                                </Link>
+                                <span>Đăng nhập</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>)
 };
-
 export default memo(Header);
