01d9ed76d1b90f6c3fabf16237c48d91a945b604
TLien <Dtlienk20@gmail.com>
2025-11-01 14:48:46 +0700

    b7

--- Fe/greentech/src/pages/user/theme/header/index.js
+++ Fe/greentech/src/pages/user/theme/header/index.js
@@ -1,54 +1,160 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import "./style.scss";
-import { AiOutlineFacebook, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineMail, AiOutlineTwitter, AiOutlineUser } from "react-icons/ai";
+import { AiOutlineFacebook, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineMail, AiOutlineShoppingCart, AiOutlineTwitter, AiOutlineUser } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { formater } from "utils/fomater";
+import { ROUTERS } from "utils/router";
 
 const Header = () => {
+    const [menus, setMenus] = useState([
+        {
+            name: "Trang chủ ",
+            path: ROUTERS.USER.HOME,
+        },
+        {
+            name: "Sản phẩm ",
+            path: ROUTERS.USER.PRODUCT,
+            isShowSubMenu: false,
+            child: [
+                {
+                    name: "Trái cây ",
+                    path: ""
+                },
+                {
+                    name: "Thực phẩm tươi sống ",
+                    path: ""
+                },
+                {
+                    name: "Sản phẩm đông lạnh",
+                    path: ""
+                },
+                {
+                    name: "Nước sốt & Gia vị ",
+                    path: ""
+                },
+                {
+                    name: "Rau củ",
+                    path: ""
+                },
+                {
+                    name: "Sản phẩm nổi bật",
+                    path: ""
+                }
+            ]
+        },
+        {
+            name: "Tin tức ",
+            path: ROUTERS.USER.NEWS,
+        },
+        {
+            name: "Giới thiệu",
+            path: ROUTERS.USER.ABOUT_US,
+        },
+        {
+            name: "Liên hệ với chúng tôi",
+            path: ROUTERS.USER.CONTACT_US,
+        }
+    ]);
     return (
-        <div className="header_top">
-            <div className="container">
-                <div className="row">
-                    <div className="col-6 header_top_left">
-                        <ul>
-                            <li>
-                                <AiOutlineMail />   Dtlienk2@gmail.com
-                            </li>
-                            <li>Miễn phí ship đơn từ {formater(200000)}</li>
-                        </ul>
-                    </div>
-                    <div className="col-6 header_top_right">
-                        <ul>
-                            <li>
-                                <Link to={""}>
-                                    <AiOutlineFacebook />
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to={""}>
-                                    <AiOutlineInstagram />
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to={""}>
-                                    <AiOutlineLinkedin />
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to={""}>
-                                    <AiOutlineTwitter />
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to={""}>
-                                    <AiOutlineUser />
-                                </Link>
-                                <span>Đăng nhập</span>
-                            </li>
-                        </ul>
+        <>
+            <div className="header_top">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6 header_top_left">
+                            <ul>
+                                <li>
+                                    <AiOutlineMail />   Dtlienk2@gmail.com
+                                </li>
+                                <li>Miễn phí ship đơn từ {formater(200000)}</li>
+                            </ul>
+                        </div>
+                        <div className="col-6 header_top_right">
+                            <ul>
+                                <li>
+                                    <Link to={""}>
+                                        <AiOutlineFacebook />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={""}>
+                                        <AiOutlineInstagram />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={""}>
+                                        <AiOutlineLinkedin />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={""}>
+                                        <AiOutlineTwitter />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={""}>
+                                        <AiOutlineUser />
+                                    </Link>
+                                    <span>Đăng nhập</span>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>)
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-3">
+                        <div className="header_logo">
+                            <Link to={"/"}>
+                                <img src="/img/Logo.png" alt="Logo" style={{ width: "180px", height: "auto" }} />
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <nav className="header_menu">
+                            <ul>
+                                {
+                                    menus?.map((menu, menuKey) => (
+                                        <li key={menuKey} className={menuKey == 0 ? "active" : ""}>
+                                            <Link to={menu.path}>{menu?.name}</Link>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                            {/* <ul>
+                                <li>
+                                    <link to=""> Trang chủ</link>
+                                </li>
+                                <li>
+                                    <link to=""> Sản phẩm</link>
+                                    <ul>
+                                        <li>
+                                            Thịt
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul> */}
+                        </nav>
+                    </div>
+                    <div className="col-xl-3">
+                        <div className="header_cart">
+                            <div className="header_cart_price">
+                                <span>{formater(100200)}</span>
+                            </div>
+                            <ul>
+                                <li>
+                                    <Link to={""}>
+                                        <AiOutlineShoppingCart />
+                                        <span>5</span>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
 };
 export default memo(Header);
--- Fe/greentech/src/utils/router.js
+++ Fe/greentech/src/utils/router.js
@@ -2,6 +2,14 @@ export const ROUTERS = {
     USER: {
         HOME: "",
         PROFILE: "profile",
+        PRODUCT: "products",
+        PRODUCT_DETAIL: "products/:id",
+        CART: "cart",
+        CHECKOUT: "checkout",   
+        NEWS: "news",
+        NEWS_DETAIL: "news/:id",
+        ABOUT_US: "about-us",
+        CONTACT_US: "contact-us",
     },
 
 };
